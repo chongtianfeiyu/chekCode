90e6eac4b528b5730b5d7040bdfd2074c43149bb
蓝月萧枫 <1752295326@qq.com>
2015-03-16 21:26:38 +0800

    例子

---
+++ demo.php
@@ -0,0 +1,27 @@
+<?php
+	session_start();
+	include"verificationCode.class.php";
+	if($_GET['w']!=''&&$_GET['h']!=''&&$_GET['b']!=''&&$_GET['n'])
+	{
+	$width=$_GET['w'];
+	$height=$_GET['h'];
+	$bool=$_GET['b'];
+	$num=$_GET['n'];
+	}
+	else
+	{
+	$width=80;
+	$height=30;
+	$bool=false;
+	$num=4;
+	}
+	$code=new verificationCode($width,$height,$num,$bool);
+	
+	//$code->showImage("Roboto-Regular.ttf");
+	//$code->showImage("../fonts/STCAIYUN.TTF");
+	$code->showImage("../fonts/简竹节.ttf");//应用字体
+	//$code->showImage("../fonts/DFPShaoNvW5-GB.ttf");//
+	//$code->showImage("../fonts/tt0970m_.ttf");//
+	//$code->showImage();//
+	$_SESSION["code"]=$code->getCheckCode();  //将验证码保存到服务器中（每个网址具有唯一性）
+	
